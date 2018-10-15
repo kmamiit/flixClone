286cbadb9b704e9f016fc2c38445c14a609cf45e
Kyle Mamiit (New) <KYLESMYLE@dhcp-v102-100.mobile.uci.edu>
2018-10-14 23:54:45 -0700

    Added collection view

---
+++ flixClone/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flixClone
+//
+//  Created by Kyle Mamiit (New) on 10/14/18.
+//  Copyright © 2018 Kyle Mamiit. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
